df24f0e31c37e4786960c64d7f64f04dae8bb079
Marissa Beatty <beattymarissa@gmail.com>
2017-07-17 23:19:09 +0000

    properly implemented and tested calculator; no longer concatenating numbers as strings

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var app = express();
 
+//example
 // app.get('/', function (req, res) {
 //   res.send('Hello World');
 // // });
 
+// exercise 1
 // app.get('/hello', function (req, res) {
 //   res.end('<h1>Hello World!</h1>');
 // });
 
+//exercise 2
 // app.get('/hello', function (req, res) {
 //   var name = req.query.name;
 //   if (name) {
@@ -22,33 +25,43 @@ var app = express();
 //note: res.send() will send the HTTP response. 
 //res.end() will end the response process
 
+//exercise 3
+//test: https://reddit-nodejs-api-marissacodes.c9users.io/calculator/add?num1=2&num2=6
 app.get('/calculator/:operation', function (req, res) {
-  var num1 = req.query.num1;
-  var num2 = req.query.num2;
+  var num1 = Number(req.query.num1); //declare num1 as a number, otherwise it concatenates when adding
+  var num2 = Number(req.query.num2);
   var operation = req.params.operation;
+  var solution; //we'll define it later
 
   if(!num1) {
     num1 = 0;
-  } if(!num2) {
-  num2 = 0;
-  } if(operation !== "add" && operation !== "multiply") {
-    res.status(400).send("Bad Request");
-  } if(operation === "add") {
-     res.send({
-      "operation": "add",
-      "firstOperand": num1,
-      "secondOperand": num2,
-      "solution": num1 + num2
-    });
-  } res.send({
-      "operation": "multiply",
-      "firstOperand": num1,
-      "secondOperand": num2,
-      "solution": num1 * num2
-    });
   } 
-// console.log(req.params); console.log(req.query);
-  // res.end(JSON.stringify());
+  
+  if(!num2) {
+  num2 = 0;
+  } 
+  
+  if(operation === "add") {
+    solution = (num1) + (num2);
+  } 
+  
+  if(operation === "multiply") {
+    solution = (num1) * (num2);
+  } 
+  
+  if(operation !== "add" && operation !== "multiply") {
+    res.status(400).send("Bad Request");
+    } 
+    
+   res.send(JSON.stringify(
+     {
+      "operation": operation,
+      "firstOperand": num1,
+      "secondOperand": num2,
+      "solution": solution
+      }
+    ));  
+  }
 );
 
 
